0d6b2f5104d161f8df1d8c47dafe5984f9242b86
thineth44 <ubandara713@gmail.com>
2024-06-30 14:40:27 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
- 
+@echo off
+set updateCommand="curl https://api.dnsexit.com/dns/ud/?apikey=r2iQA5lRl2J1Z479Y71qS87s81U4hg -d host=thineth.work.gd"
+rem Create a scheduled task to run every 12 minutes. It must be run as System Administrator because it uses the SYSTEM account..
+schtasks /create /tn "DNS Exit IP Update" /tr %updateCommand% /sc minute /mo 12 /ru SYSTEM
+echo DNS Exit IP Update Task created. 
